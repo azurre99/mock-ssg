48ed3a4f5e47d38a14eaad93d18110c16fb0b071
Azur Kurbasic <azurresolutions@gmail.com>
2024-10-24 16:29:30 +0200

    Update stupid config

--- middleware/redirectTrailingSlash.global.ts
+++ middleware/redirectTrailingSlash.global.ts
@@ -3,6 +3,7 @@ export default defineNuxtRouteMiddleware((to) => {
         const { path, query, hash } = to;
         const nextPath = path + '/';
         const nextRoute = { path: nextPath, query, hash };
+        console.log(`Not ending with slash, navigating to ${nextPath}, ${query}, ${hash}`)
         return navigateTo(nextRoute, { redirectCode: 301 });
     }
 });
