b2c90a047da90b0c32f41187daef6a76b134bc23
Azur Kurbasic <azurresolutions@gmail.com>
2024-10-24 16:38:25 +0200

    Minor update

--- middleware/redirectTrailingSlash.global.ts
+++ middleware/redirectTrailingSlash.global.ts
@@ -1,7 +1,8 @@
 export default defineNuxtRouteMiddleware((to) => {
-    if (!to.path.endsWith('/')) {
+    // If the path is not '/' and doesn't already end with a trailing slash
+    if (to.path !== '/' && !to.path.endsWith('/')) {
         const { path, query, hash } = to;
-        const nextPath = path + '/';
+        const nextPath = path + '/'; // Add trailing slash
         const nextRoute = { path: nextPath, query, hash };
         console.log(`Not ending with slash, navigating to ${nextPath}, ${query}, ${hash}`)
         return navigateTo(nextRoute, { redirectCode: 301 });
