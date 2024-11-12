4814904cafcbbd002cd653889a9d2297eb3106b9
Azur Kurbasic <azurresolutions@gmail.com>
2024-11-12 15:45:02 +0100

    Update

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 
+import {Buffer} from "unenv/runtime/node/buffer/_buffer";
+import from = Buffer.from;
+
 const router = useRouter();
 const route = useRoute();
 
@@ -17,14 +20,20 @@ let lastPart = pageSplit.length > 0 ? pageSplit[pageSplit.length - 1] : ""
 
 const mathRandom = Math.floor(Math.random() * 10)
 const { data } = await useFetch(`/api/page/${lastPart}`);
+const arr = Array.from({length: 10}).map((_, i) => i)
 </script>
 
 <template>
   <div>
+    Currently on: {{ mathRandom }}
     <ClientOnly>
-      <NuxtLink :to="`/something/${mathRandom}/`">
-        Navigate to {{ mathRandom }}
-      </NuxtLink>
+      <ul>
+        <li v-for="item in arr">
+          <NuxtLink :to="`/something/${item}/`">
+            Navigate to {{ item }}
+          </NuxtLink>
+        </li>
+      </ul>
     </ClientOnly>
     <div>
       {{ data }}
