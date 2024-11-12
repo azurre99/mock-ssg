5c90d7fd0f54176ccd84a36b09422993d42323ef
Azur Kurbasic <azurresolutions@gmail.com>
2024-11-12 14:19:09 +0100

    Page updated

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -1,6 +1,6 @@
 export default defineEventHandler(async (event) => {
     const url = getRequestURL(event)
-    console.log(`Fetching url ${url.href}`)
+    console.log(`Fetching url ${url.href}, ${url.searchParams}`)
 
     const todo = Math.ceil(Math.random() * 201)
     return await $fetch(`https://jsonplaceholder.typicode.com/todos/${todo}`)
