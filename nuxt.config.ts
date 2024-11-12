a55a230fa4a42d7b502b28979e1a10f9f30e9129
Azur Kurbasic <azurresolutions@gmail.com>
2024-11-12 15:10:57 +0100

    Test 1231

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,25 +8,12 @@ export default defineNuxtConfig({
         },
     },
     compatibilityDate: '2024-04-03',
-    ssr: true,
+    routeRules: {
+        "/**": { isr: true, ssr: true }
+    },
     router: {
         options: {
             strict: false
-        }
-    },
-    hooks: {
-        async "nitro:config"(nitroConfig) {
-            const newRules = {
-                "/something/1": { isr: true },
-                "/something/2": { isr: true },
-                "/something/3": { isr: true },
-                "/something/4/": { isr: true },
-                "/something/5/": { isr: true },
-            }
-            nitroConfig.routeRules = {
-                ...nitroConfig.routeRules,
-                ...newRules
-            }
         }
     }
 })
